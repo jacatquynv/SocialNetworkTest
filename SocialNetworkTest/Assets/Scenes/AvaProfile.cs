0db0063e966abedf0fea969698de5ba72dd1f87d
jacatquynv <quynv@jacatgames.com>
2022-01-19 16:46:09 +0700

    complete

--- SocialNetworkTest/Assets/Scenes/AvaProfile.cs
+++ SocialNetworkTest/Assets/Scenes/AvaProfile.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using Firebase.Extensions;
+using System.Collections.Generic;
+using LitJson;
 
 public class AvaProfile : MonoBehaviour
 {
@@ -14,10 +16,13 @@ public class AvaProfile : MonoBehaviour
     [SerializeField] Button like;
 
     StorageReference m_storage;
+    DatabaseReference database;
+
+    UserData oldData;
 
     public void Init(User user)
     {
-        //database = FirebaseDatabase.GetInstance("https://socialnetworktest-c60c5-default-rtdb.asia-southeast1.firebasedatabase.app/").GetReference("socialnetworktest-c60c5-default-rtdb");
+        database = FirebaseDatabase.GetInstance("https://socialnetworktest-c60c5-default-rtdb.asia-southeast1.firebasedatabase.app/").GetReference("socialnetworktest-c60c5-default-rtdb");
         StorageReference storage = FirebaseStorage.DefaultInstance.GetReferenceFromUrl($"gs://socialnetworktest-c60c5.appspot.com/{user.data.urlImage}");
        // StorageReference storage = m_storage.Child(user.data.urlImage);
 
@@ -32,6 +37,13 @@ public class AvaProfile : MonoBehaviour
 
         m_name.text = user.data.name;
         numberLike.text = user.data.numberLike.ToString();
+
+        database = database.Child($"share_image/{user.id}");
+        database.ValueChanged += HandleValueChanged;
+
+        like.onClick.AddListener(OnClickLike);
+
+        Debug.Log(database.Push().Key);
     }
 
     IEnumerator GetTexture(string url)
@@ -48,5 +60,27 @@ public class AvaProfile : MonoBehaviour
             Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
             m_image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100f);
         }
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        // Do something with the data in args.Snapshot
+
+        oldData = JsonMapper.ToObject<UserData>(args.Snapshot.GetRawJsonValue());
+        numberLike.text = oldData.numberLike.ToString();
+    }
+
+    void OnClickLike()
+    {
+        int oldLike = oldData.numberLike;
+        Dictionary<string, object> newLike = new Dictionary<string, object>();
+        newLike.Add("numberLike", oldLike + 1);
+
+        database.UpdateChildrenAsync(newLike);
     }
 }
